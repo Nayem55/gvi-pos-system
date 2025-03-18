759c02a99d1a8d6068fcedab08b0c8da82e47951
Nayem55 <nayem.sayed.web@gmail.com>
2025-03-18 15:35:58 +0600

    ..

--- src/Pages/UserStock.js
+++ src/Pages/UserStock.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 
@@ -7,40 +7,47 @@ const ManageUserStock = () => {
   const [selectedOutlet, setSelectedOutlet] = useState(user?.outlet || "");
   const [products, setProducts] = useState([]);
   const [stocks, setStocks] = useState({});
-  const [originalStocks, setOriginalStocks] = useState({}); // Store initial stocks
+  const [originalStocks, setOriginalStocks] = useState({});
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
   const [searchQuery, setSearchQuery] = useState("");
   const [page, setPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
-  const [updating, setUpdating] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const observer = useRef();
 
   useEffect(() => {
     if (selectedOutlet) {
-      fetchProducts();
+      setProducts([]); // Clear products on outlet change or search
+      setPage(1); // Reset page to 1 for fresh fetch
+      setHasMore(true); // Reset pagination
+      fetchProducts(1);
     }
-  }, [selectedOutlet, page, searchQuery]);
+  }, [selectedOutlet, searchQuery]);
 
-  const fetchProducts = async () => {
+  const fetchProducts = async (pageNumber) => {
+    if (!hasMore && !searchQuery) return;
     setLoading(true);
     try {
       let response;
       let fetchedProducts;
 
       if (searchQuery) {
+        // Search query, disable pagination
         response = await axios.get(
           `https://gvi-pos-server.vercel.app/search-product?search=${searchQuery}&type=name`
         );
         fetchedProducts = response.data;
+        setHasMore(false); // No more products for pagination if searching
       } else {
+        // Regular fetch with pagination
         response = await axios.get(
-          `https://gvi-pos-server.vercel.app/products?page=${page}`
+          `https://gvi-pos-server.vercel.app/products?page=${pageNumber}`
         );
         fetchedProducts = response.data.products;
-        setTotalPages(response.data.totalPages);
+        setHasMore(response.data.products.length > 0);
       }
 
-      setProducts(fetchedProducts);
+      setProducts((prev) => [...prev, ...fetchedProducts]);
 
       const stockData = {};
       const originalStockData = {};
@@ -52,22 +59,41 @@ const ManageUserStock = () => {
           );
           const stockValue = stockResponse.data.stock || 0;
           stockData[product.barcode] = stockValue;
-          originalStockData[product.barcode] = stockValue; // Store original stock
+          originalStockData[product.barcode] = stockValue;
         } catch (error) {
-          console.error(`Error fetching stock for ${product.barcode}:`, error);
           stockData[product.barcode] = 0;
           originalStockData[product.barcode] = 0;
         }
       });
 
       await Promise.all(stockRequests);
-      setStocks(stockData);
-      setOriginalStocks(originalStockData); // Save original stocks
+      setStocks((prev) => ({ ...prev, ...stockData }));
+      setOriginalStocks((prev) => ({ ...prev, ...originalStockData }));
     } catch (error) {
       console.error("Error fetching products or stocks:", error);
     }
     setLoading(false);
   };
+
+  const lastProductRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore && !searchQuery) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore, searchQuery]
+  );
+
+  useEffect(() => {
+    if (page > 1 && !searchQuery) {
+      fetchProducts(page);
+    }
+  }, [page, searchQuery]);
 
   const handleStockChange = (barcode, value) => {
     setStocks((prevStocks) => ({
@@ -77,38 +103,27 @@ const ManageUserStock = () => {
   };
 
   const updateStock = async (barcode) => {
-    setUpdating(true);
     try {
       await axios.put("https://gvi-pos-server.vercel.app/update-outlet-stock", {
         barcode,
         outlet: selectedOutlet,
         newStock: stocks[barcode],
       });
-  
       toast.success("Stock updated successfully");
-  
-      setOriginalStocks((prevOriginalStocks) => ({
-        ...prevOriginalStocks,
-        [barcode]: stocks[barcode],
-      }));
-  
-      setUpdating(false);
+      setOriginalStocks((prev) => ({ ...prev, [barcode]: stocks[barcode] }));
     } catch (error) {
       console.error("Error updating stock:", error);
       toast.error("Failed to update stock");
-      setUpdating(false);
     }
   };
-  
 
   return (
     <div className="flex sm:p-10 p-4">
       <div className="w-full">
         <div className="mb-4 flex flex-col sm:flex-row justify-between items-center gap-4">
-          <h2 className="text-xl sm:text-2xl font-bold text-left sm:text-left">
+          <h2 className="text-xl sm:text-2xl font-bold">
             Manage Stock: {selectedOutlet}
           </h2>
-
           <div className="flex flex-col sm:flex-row gap-2 w-full sm:w-auto">
             <input
               type="text"
@@ -118,108 +133,70 @@ const ManageUserStock = () => {
               className="border p-2 rounded w-full sm:w-64"
             />
             <button
-              onClick={() => setSearchQuery(search)}
+              onClick={() => {
+                setSearchQuery(search);
+                setPage(1); // Reset page on new search
+              }}
               className="bg-gray-800 text-white px-4 py-2 rounded w-full sm:w-auto"
             >
               Search
             </button>
           </div>
         </div>
-
-        {loading ? (
-          <p className="text-center">Loading...</p>
-        ) : (
-          <>
-            <div className="overflow-x-auto">
-              <table className="w-full border-collapse border border-gray-300 text-sm sm:text-base">
-                <thead>
-                  <tr className="bg-gray-100 text-left">
-                    <th className="border p-2">Barcode</th>
-                    <th className="border p-2">Product Name</th>
-                    <th className="border p-2">Opening Stock</th>
-                    <th className="border p-2">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {products.map((product) => (
-                    <tr key={product.barcode} className="border">
-                      <td className="border p-2">{product.barcode}</td>
-                      <td className="border p-2">{product.name}</td>
-                      <td className="border p-2">
-                        <input
-                          type="number"
-                          value={stocks[product.barcode]}
-                          onChange={(e) =>
-                            handleStockChange(
-                              product.barcode,
-                              parseInt(e.target.value)
-                            )
-                          }
-                          className="border p-1 w-full text-center"
-                        />
-                      </td>
-                      <td className="border p-2">
-                        <button
-                          onClick={() => updateStock(product.barcode)}
-                          disabled={originalStocks[product.barcode] > 0 || updating} 
-                          className={`px-3 py-1 rounded-md w-full sm:w-auto ${
-                            originalStocks[product.barcode] > 0 || updating
-                              ? "bg-gray-400 cursor-not-allowed text-white"
-                              : "bg-green-500 hover:bg-gray-800 text-white"
-                          }`}
-                        >
-                          {updating ? (
-                            <svg
-                              className="animate-spin h-5 w-5 text-white mx-auto"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                            >
-                              <circle
-                                className="opacity-25"
-                                cx="12"
-                                cy="12"
-                                r="10"
-                                stroke="currentColor"
-                                strokeWidth="4"
-                              ></circle>
-                              <path
-                                className="opacity-75"
-                                fill="currentColor"
-                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
-                              ></path>
-                            </svg>
-                          ) : (
-                            "Update"
-                          )}
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+        <div className="overflow-x-auto">
+          <table className="w-full border-collapse border border-gray-300 text-sm sm:text-base">
+            <thead>
+              <tr className="bg-gray-100 text-left">
+                <th className="border p-2">Barcode</th>
+                <th className="border p-2">Product Name</th>
+                <th className="border p-2">Opening Stock</th>
+                <th className="border p-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => (
+                <tr
+                  key={product.barcode}
+                  ref={products.length - 1 === index ? lastProductRef : null}
+                  className="border"
+                >
+                  <td className="border p-2">{product.barcode}</td>
+                  <td className="border p-2">{product.name}</td>
+                  <td className="border p-2">
+                    <input
+                      type="number"
+                      value={stocks[product.barcode]}
+                      onChange={(e) =>
+                        handleStockChange(
+                          product.barcode,
+                          parseInt(e.target.value)
+                        )
+                      }
+                      className="border p-1 w-full text-center"
+                    />
+                  </td>
+                  <td className="border p-2">
+                    <button
+                      onClick={() => updateStock(product.barcode)}
+                      disabled={originalStocks[product.barcode] > 0}
+                      className={`px-3 text-white py-1 rounded-md w-full sm:w-auto ${
+                        originalStocks[product.barcode] > 0
+                          ? "bg-gray-400 cursor-not-allowed"
+                          : "bg-green-500 hover:bg-gray-800 "
+                      }`}
+                    >
+                      Update
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {loading && (
+            <div className="flex justify-center items-center my-10">
+              <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
             </div>
-
-            <div className="mt-4 flex gap-4 justify-center">
-              <button
-                disabled={page === 1}
-                onClick={() => setPage((prev) => prev - 1)}
-                className="bg-gray-300 px-3 py-1 rounded disabled:opacity-50"
-              >
-                Previous
-              </button>
-              <span>
-                Page {page} of {totalPages}
-              </span>
-              <button
-                disabled={page === totalPages}
-                onClick={() => setPage((prev) => prev + 1)}
-                className="bg-gray-300 px-3 py-1 rounded disabled:opacity-50"
-              >
-                Next
-              </button>
-            </div>
-          </>
         )}
       </div>
     </div>
